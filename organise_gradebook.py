9c933348bd6433b55446207a038a53e2ff38301a
vangef <vangef@outlook.com>
2023-03-01 00:46:04 +0000

    remove_trailing_spaces_from_filenames() not fixing the error - removed

--- organise_gradebook.py
+++ organise_gradebook.py
@@ -1,5 +1,5 @@
 import os, sys
-from utils.organiser import organise_gradebook, check_submissions_dir_for_compressed, remove_trailing_spaces_from_filenames
+from utils.organiser import organise_gradebook, check_submissions_dir_for_compressed
 
 def main():
     gradebook_name = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else exit(f'\nNo gradebook name given. Provide the name as an argument.\n\nUsage: python {sys.argv[0]} [gradebook dir name]\n')
@@ -9,7 +9,6 @@ def main():
     abs_path = os.getcwd()  # absolute path of main/this script
     print(f'\nGradebook directory to organise: {os.path.join(abs_path, gradebook_dir)}')
     
-    remove_trailing_spaces_from_filenames(gradebook_dir)
     organise_gradebook(gradebook_dir, submissions_dir)
     check_submissions_dir_for_compressed(submissions_dir) 
 
