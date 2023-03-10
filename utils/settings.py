e1ef82fa18fd2c899503075b74100f26bc3503fc
vangef <vangef@outlook.com>
2023-03-10 12:47:15 +0000

    moved constants to settings.py

---
+++ utils/settings.py
@@ -0,0 +1,4 @@
+import os
+
+BAD_DIR_NAME = '__BAD__'  # for organise_gradebook.py - directory with corrupt/invalid compressed files
+CSV_DIR = os.path.join(os.getcwd(), 'csv-inspect')  # for inspect_gradebook.py and inspect_submissions.py - output dir for generated CSV files
